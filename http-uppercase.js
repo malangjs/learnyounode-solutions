7fcf96dcbd2d92393775cb18a892198c96532230
Khalilur Rahman <khal.rahman@gmail.com>
2015-02-14 00:49:40 +0800

    added http uppercaser solution

---
+++ http-uppercase.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var map = require('through2-map');
+
+var portNumber = process.argv[2];
+var server = http.createServer(function(req, res) {
+	if (req.method === 'POST') {
+		req.pipe(map(function(data){
+			return data.toString().toUpperCase();
+		})).pipe(res);
+	}
+});
+server.listen(portNumber);
